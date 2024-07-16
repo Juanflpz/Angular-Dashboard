16f755ff315db1df7bd8c57a87935795e5b1b07b
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-07-16 03:00:19 -0500

    managing control flow

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'F';
 @Component({
   standalone: true,
   imports: [CommonModule],
@@ -11,6 +12,10 @@ export default class ControlFlowComponent {
 
   //asReadOnly()
   public showContent = signal(false)
+  public grade = signal<Grade>('F');
+
+  public frameworks = signal(['Angular', 'React', 'Vue', 'Svelte', 'Ember']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update(value => !value);

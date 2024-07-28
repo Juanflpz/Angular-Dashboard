40a9410a773ab4ced883aea118215dc4c186e979
Juan Felipe López <juanfelipelopez55@gmail.com>
2024-07-27 21:14:03 -0500

    defers done

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@app/shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@app/shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
   template: `
-  <h1>hola</h1>
+  <section [ngClass]="['w-full', cssClass]">
+    <ng-content/>
+  </section>
   `
 })
 
 export class HeavyLoadersFastComponent {
+  @Input({required: true}) cssClass!: string;
 
+  constructor() {
+    console.log('HeavyLoadersFastComponent created');
+  }
 }
